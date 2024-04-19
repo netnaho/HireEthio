23226e5fc7051d592b6b1ba40bf4e7adc8cbfdbd
netnaho <87896136+netnaho@users.noreply.github.com>
2024-04-20 01:16:48 +0300

    setting up the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-center items-center h-[71vh]">
+      <div className="w-[30%] mx-auto shadow-md shadow-slate-500 p-7 rounded-lg">
+        <h1 className="text-center text-3xl font-bold mb-3">
+          Login to your Account
+        </h1>
+        <form action="" method="get">
+          <label className="font-medium" htmlFor="username">
+            Username:
+          </label>
+          <Input
+            className="mt-2"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+          <br />
+          <label className="font-medium" htmlFor="password">
+            Password:
+          </label>
+          <Input
+            className="mt-2"
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+          <div className="flex justify-center items-center mt-6">
+            <Button className="w-full bg-[#38A3A5] rounded-3xl">Log in</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
