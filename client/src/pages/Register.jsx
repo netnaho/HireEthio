d66cf1cf0d807bb07720810460f2f92697976a40
netnaho <87896136+netnaho@users.noreply.github.com>
2024-06-18 13:35:46 +0300

    Register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Button } from "@/components/ui/button";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [userType, setUserType] = useState(null);
+  return (
+    <div className="h-[70vh] flex flex-col gap-y-10 justify-center items-center">
+      <div>
+        <h1 className="text-5xl text-center font-mono font-semibold text-green-700">
+          Which role describes
+          <br /> your account ?
+        </h1>
+      </div>
+      <div className="flex justify-center items-center h-fit">
+        <RadioGroup className="flex gap-x-10" defaultValue="comfortable">
+          <div
+            onClick={() => {
+              setUserType("freelancer");
+            }}
+            className={`${
+              userType === "freelancer" &&
+              "duration-300 shadow-lg shadow-green-800 border-green-500"
+            } flex shadow-md shadow-slate-600 border-[1px] border-slate-300 p-4 w-[160px] h-[160px] rounded-lg gap-x-6 space-x-2`}
+          >
+            <RadioGroupItem
+              checked={userType === "freelancer" && true}
+              value="default"
+              id="r1"
+            />
+            <Label htmlFor="r1">Freelancer</Label>
+          </div>
+          <div
+            onClick={() => {
+              setUserType("client");
+            }}
+            className={`${
+              userType === "client" &&
+              "shadow-lg shadow-green-800 border-green-500 duration-300"
+            } flex shadow-md shadow-slate-600 border-[1px] border-slate-300 p-4 w-[160px] h-[160px] rounded-lg gap-x-6 space-x-2`}
+          >
+            <RadioGroupItem
+              checked={userType === "client" && true}
+              value="comfortable"
+              id="r2"
+            />
+            <Label htmlFor="r2">Client</Label>
+          </div>
+        </RadioGroup>
+      </div>
+      <Link
+        to={
+          userType
+            ? userType === "freelancer"
+              ? "/freelancer-signup"
+              : "/client-signup"
+            : "/register"
+        }
+      >
+        <Button className="bg-[#38A3A5]">Continue</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Register;
