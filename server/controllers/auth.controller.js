0ed5e6b5d55e4d9de4e3fdce3a1c344def8ab5e9
netnaho <87896136+netnaho@users.noreply.github.com>
2024-07-07 21:10:18 +0300

    login and register

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,3 +1,84 @@
 import { pool } from "../config/db.js"; // Importing the pool for database connection
 
-export const handleFreelancerRegister = async (req, res) => {};
+export const handleFreelancerRegister = async (req, res) => {
+  const { firstname, lastname, username, email, password, proffession, bio } =
+    req.body;
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM freelancer WHERE Email = ?",
+      [email]
+    );
+    if (rows.length > 0) {
+      return res.status(400).json({ msg: "Freelancer already exists" });
+    }
+
+    const [result] = await pool.query(
+      `INSERT INTO freelancer (FirstName, LastName, Username, Email, Password, Proffession, Bio)
+VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [firstname, lastname, username, email, password, proffession, bio]
+    );
+    console.log(result);
+    return res.json({ msg: "Freelancer registered successfully" });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const handleClientRegister = async (req, res) => {
+  const { firstname, lastname, username, email, password, clientType } =
+    req.body;
+  try {
+    const [rows] = await pool.query("SELECT * FROM client WHERE Email = ?", [
+      email,
+    ]);
+    if (rows.length > 0) {
+      return res.status(400).json({ msg: "Client already exists" });
+    }
+
+    const [result] = await pool.query(
+      `INSERT INTO client (FirstName, LastName, Username, Email, Password, Client_Type)
+VALUES (?, ?, ?, ?, ?, ?)`,
+      [firstname, lastname, username, email, password, clientType]
+    );
+    console.log(result);
+    return res.json({ msg: "Client registered successfully" });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const handleLogin = async (req, res) => {
+  const { email, password, isClient } = req.body;
+  try {
+    if (isClient) {
+      const [rows] = await pool.query("SELECT * FROM client WHERE Email = ?", [
+        email,
+      ]);
+      if (rows.length === 0) {
+        return res.status(400).json({ msg: "No such client" });
+      }
+      console.log(rows);
+      if (rows[0].Password == password) {
+        return res.json(rows);
+      } else {
+        return res.status(400).json("login failed");
+      }
+    } else {
+      const [rows] = await pool.query(
+        "SELECT * FROM freelancer WHERE Email = ?",
+        [email]
+      );
+      if (rows.length === 0) {
+        return res.status(400).json({ msg: "No such freelancer" });
+      }
+      console.log(rows);
+      if (rows[0].Password == password) {
+        return res.json(rows);
+      } else {
+        return res.status(400).json("login failed");
+      }
+    }
+  } catch (error) {
+    console.log("");
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,35 +1,21 @@
 import express from "express";
-import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 import authRoute from "./routes/auth.route.js";
-import jobRoute from "./routes/job.route.js"
+import jobRoute from "./routes/job.route.js";
 
 const app = express();
+
 const port = 8000;
 
 // Middleware, routes, etc.
 // For example, you can use express.json() to parse JSON bodies
 app.use(express.json());
+app.use(cors());
+
 app.use("/api/auth", authRoute);
 app.use("/api/job", jobRoute);
 
-// Example route
-app.get("/", (req, res) => {
-    res.send("Starting Server");
+app.listen(8800, () => {
+  console.log("Connected to backend.");
 });
-
-
-// Initialize the connection to the database and start the server
-const startServer = async () => {
-    try {
-        await connectDB(); // Wait for the database connection to be established
-        app.listen(port, () => {
-            console.log(`Backend server is running on port ${port}`);
-
-        });
-    } catch (error) {
-        console.error("Failed to start the server Becuase of:", error);
-    }
-};
-
-startServer();
