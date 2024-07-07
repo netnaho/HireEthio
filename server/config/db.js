f4b3f56e61dfaa689f0985f36687812c55d31b8c
netnaho <87896136+netnaho@users.noreply.github.com>
2024-07-07 11:44:31 +0300

    db

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,11 @@
-    import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
 
-
-    export const pool = mysql.createPool({
-        host:"localhost",
-        user:"root",
-        password: "",
-        database: "hireethio",
-        connectionLimit: 10,
-        waitForConnections: true,
-        queueLimit: 0,
-    });
-
-    export const connectDB = async () => {
-    try {
-        const connection = await pool.getConnection();
-        console.log('MySQL connected with connection pooling');
-        connection.release();
-    } catch (error) {
-        console.error('Unable to connect to the database Becuase of:', error);
-    }
-    };
-
+export const connectDB = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL connected with connection pooling");
+    connection.release();
+  } catch (error) {
+    console.error("Unable to connect to the database Becuase of:", error);
+  }
+};
